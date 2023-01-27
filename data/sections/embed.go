7251d40e9fae87d7581609c3c4a0cc9e49e88ebb
AH-dark <ahdark@outlook.com>
2023-01-27 19:30:19 +0800

    Fix: embed texts which makes the program independent of the file

---
+++ data/sections/embed.go
@@ -0,0 +1,9 @@
+package sections
+
+import _ "embed"
+
+//go:embed fabing.txt
+var FabingText string
+
+//go:embed fadian.txt
+var FadianText string
--- service/fabing/init.go
+++ service/fabing/init.go
@@ -1,21 +1,11 @@
 package fabing
 
 import (
-	"os"
-
-	"go.uber.org/zap"
-
-	"github.com/afadian/fadian-go/internal/util"
+	"github.com/afadian/fadian-go/data/sections"
 )
 
 type BaseText string
 
-func Initialize() (BaseText, error) {
-	f, err := os.ReadFile(util.AbsolutePath("data/sections/fabing.txt"))
-	if err != nil {
-		zap.L().Error("read fabing.txt failed", zap.Error(err))
-		return "", err
-	}
-
-	return BaseText(f), nil
+func Initialize() BaseText {
+	return BaseText(sections.FabingText)
 }
